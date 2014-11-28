f7e8186697cfc1e612cab9d3aad3382e39deb113
Andrew Kozlov <adkozlov@gmail.com>
2014-11-29 01:36:23 +0300

    where cursor rewritten

--- src/cursors/WhereCursor.java
+++ src/cursors/WhereCursor.java
@@ -1,45 +1,58 @@
 package cursors;
 
-import expression.Expression;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import queries.SecondLevelId;
+import queries.WhereCondition;
 
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * tiny_database
  * Created by Sergey on 09.11.2014.
  */
-public class WhereCursor implements Iterator<Object[]> {
+public class WhereCursor implements Iterator<Map<SecondLevelId, Object>> {
 
-    private final Iterator<Object[]> baseCursor;
-    private final Expression expression;
-    private Object[] value;
+    @NotNull
+    private final Iterator<Map<SecondLevelId, Object>> baseCursor;
+    @NotNull
+    private final WhereCondition condition;
+    @Nullable
+    private Map<SecondLevelId, Object> values;
 
-    public WhereCursor(Iterator<Object[]> baseCursor, Expression expression) {
-        this.expression = expression;
+    public WhereCursor(@NotNull Iterator<Map<SecondLevelId, Object>> baseCursor, @NotNull WhereCondition condition) {
         this.baseCursor = baseCursor;
-        value = getValue();
+        this.condition = condition;
+
+        values = getValues();
     }
 
-    private Object[] getValue() {
-        while(baseCursor.hasNext()) {
-        	Object[] val = baseCursor.next();
-            if (expression.check(val)) {
-                return val;
+    @Nullable
+    private Map<SecondLevelId, Object> getValues() {
+        while (baseCursor.hasNext()) {
+            Map<SecondLevelId, Object> values = baseCursor.next();
+
+            if (condition.check(values)) {
+                return values;
             }
         }
+
         return null;
     }
 
     @Override
     public boolean hasNext() {
-        return value != null;
+        return values != null;
     }
 
+    @Nullable
     @Override
-    public Object[] next() {
-    	Object[] old = value;
-        value = getValue();
-        return old;
+    public Map<SecondLevelId, Object> next() {
+        Map<SecondLevelId, Object> oldValues = values;
+        values = getValues();
+
+        return oldValues;
     }
 
     @Override
