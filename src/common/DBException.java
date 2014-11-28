d697b42ea211a0e301715e9a291c098caf90badf
Andrew Kozlov <adkozlov@gmail.com>
2014-11-29 01:09:08 +0300

    new exception constructors

--- src/common/DBException.java
+++ src/common/DBException.java
@@ -8,4 +8,12 @@ public class DBException extends RuntimeException {
     public DBException(String message) {
         super(message);
     }
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DBException(Throwable cause) {
+        super(cause);
+    }
 }
