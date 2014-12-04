ee3192f2503c058bd1ffbfb759a282edcb256935
Andrew Kozlov <adkozlov@gmail.com>
2014-12-05 02:59:08 +0300

    sql file for script run

---
+++ testData/execution/deanery.sql
@@ -0,0 +1,28 @@
+CREATE TABLE students (
+  student_id INTEGER,
+  first_name VARCHAR(10),
+  last_name VARCHAR (15),
+  avg_point DOUBLE
+);
+
+CREATE TABLE groups (
+  group_id INTEGER,
+  group_name VARCHAR(3)
+);
+
+CREATE TABLE student_groups (
+  student_id INTEGER,
+  group_id INTEGER
+);
+
+INSERT INTO students(student_id, avg_point, first_name, last_name) VALUES (3, 4.8, 'Andrew', 'Kozlov');
+
+INSERT INTO groups(group_id, group_name) VALUES (1, '5SE');
+
+INSERT INTO student_groups(student_id, group_id) VALUES (3, 1);
+
+SELECT * FROM students;
+
+SELECT first_name, last_name, group_name FROM students INNER JOIN student_groups ON students->student_id = student_groups->student_id INNER JOIN groups ON student_groups->group_id = groups->group_id;
+
+SELECT first_name, last_name FROM students WHERE students->student_id >= 0 AND students->student_id < 100;
