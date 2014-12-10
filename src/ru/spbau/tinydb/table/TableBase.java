7049cdc52b69955d96d50061c9c2e3a39dc834c0
SergeyKrivohatskiy <s.krivohatskiy@gmail.com>
2014-12-10 19:46:49 +0300

    Deleted flag added to TableBase. Index may be used now without reindexing on delete. 'vacuum' method isn't implemented
    Signed-off-by: SergeyKrivohatskiy <s.krivohatskiy@gmail.com>

--- src/ru/spbau/tinydb/table/TableBase.java
+++ src/ru/spbau/tinydb/table/TableBase.java
@@ -30,8 +30,11 @@ public class TableBase implements Iterable<ViewWithId> {
         firstFullPage = 0;
         firstNotFullPage = 0;
         rowsCount = 0;
-        recordsPerPage = (int)(8 * (BufferManager.PAGE_SIZE - POINTERS_SIZE)) / (8 * recordSize + 1);
-        bitSetSize = (recordsPerPage + 7) / 8;
+        // (Available bits count) / (bits per record)
+        // bits per record is 8 * recordSize plus 2 bits for 
+        // 2 bitSets(availability and delete flag)
+        recordsPerPage = (int)(8 * (BufferManager.PAGE_SIZE - POINTERS_SIZE)) / (8 * recordSize + 2);
+        bitSetSize = (recordsPerPage + recordsPerPage + 7) / 8;
         loadHeader();
     }
 
@@ -75,18 +78,18 @@ public class TableBase implements Iterable<ViewWithId> {
         BufferView recordsView = pageView.getSubView(POINTERS_SIZE + bitSetSize, recordsPerPage * recordSize);
         int recordPosition;
         // Hand made BitSet
-        for(recordPosition = 0; (bitSetView.getByte(recordPosition / 8) & (1 << (recordPosition % 8))) != 0; recordPosition ++) {
+        for(recordPosition = 0; checkBit(bitSetView, recordPosition); recordPosition ++) {
             if(recordPosition >= recordsPerPage) {
                 throw new RuntimeException("No empty position when page is not full");
             }
         }
-        bitSetView.setByte(recordPosition / 8, (byte) (bitSetView.getByte(recordPosition / 8) | (1 << (recordPosition % 8))));
+        setBit(bitSetView, recordPosition);
         recordsView.setBytes(recordPosition * recordSize, row);
         recordsView.close();
 
         boolean bitSetFull = true;
         for(int i = 0; i < recordsPerPage; i ++) {
-            if((bitSetView.getByte(i / 8) >> (i % 8) & 1) == 0) {
+            if(!checkBit(bitSetView, i)) {
                 bitSetFull = false;
                 break;
             }
@@ -130,25 +133,34 @@ public class TableBase implements Iterable<ViewWithId> {
         writeHeader();
         return pageIndex * recordsPerPage + recordPosition;
     }
+
+
+	private void setBit(BufferView bitSetView, int recordPosition) {
+		bitSetView.setByte(recordPosition / 8, (byte) (bitSetView.getByte(recordPosition / 8) | (1 << (recordPosition % 8))));
+	}
+
+
+	private boolean checkBit(BufferView bitSetView, int recordPosition) {
+		return (bitSetView.getByte(recordPosition / 8) & (1 << (recordPosition % 8))) != 0;
+	}
     
     public boolean remove(int recordId) {
-        // TODO find out the better way of removing. This requires index rebuilding(because this recordId may be reused)
         int pageIndex = recordId / recordsPerPage;
         int recordPosition = recordId % recordsPerPage;
         try (BufferView pageView = bufferManager.getPage(pageIndex)) {
             BufferView bitSetView = pageView.getSubView(POINTERS_SIZE, bitSetSize);
-            byte oldByte = bitSetView.getByte(recordPosition / 8);
-            boolean recordAvailable = (oldByte & 1 << (recordPosition % 8)) != 0;
-            // TODO test this
-            bitSetView.setByte(recordPosition / 8, (byte) (oldByte & ~(1 << (recordPosition % 8))));
+            boolean recordAvailable = checkBit(bitSetView, recordPosition);
+            boolean recordDeleted = checkBit(bitSetView, recordsPerPage + recordPosition);
+            if(!recordDeleted) {
+            	setBit(bitSetView, recordsPerPage + recordPosition);
+            	bitSetView.setChanged();
+            }
             bitSetView.close();
             
-            if (!recordAvailable) {
+            if (!recordAvailable || recordDeleted) {
                 return false;
             }
             
-            // TODO if page was full- move to notFullPages
-            pageView.setChanged();
             rowsCount -= 1;
             writeHeader();
             return true;
@@ -160,9 +172,10 @@ public class TableBase implements Iterable<ViewWithId> {
         int recordPosition = recordId % recordsPerPage;
         try (BufferView pageView = bufferManager.getPage(pageIndex)) {
             BufferView bitSetView = pageView.getSubView(POINTERS_SIZE, bitSetSize);
-            boolean recordAvailable = (bitSetView.getByte(recordPosition / 8) & 1 << (recordPosition % 8)) != 0;
+            boolean recordAvailable = checkBit(bitSetView, recordPosition);
+            boolean recordDeleted = checkBit(bitSetView, recordsPerPage + recordPosition);
             bitSetView.close();
-            if (recordAvailable) {
+            if (recordAvailable && !recordDeleted) {
                 return pageView.getSubView(POINTERS_SIZE + bitSetSize + recordPosition * recordSize, recordSize);
             }
             return null;
@@ -197,7 +210,9 @@ public class TableBase implements Iterable<ViewWithId> {
                     }
                     BufferView bitSetView = pageView.getSubView(POINTERS_SIZE, bitSetSize);
                     while(recordPos != recordsPerPage) {
-                        if(((bitSetView.getByte(recordPos / 8) >> (recordPos % 8)) & 1) != 0) {
+                        boolean recordAvailable = checkBit(bitSetView, recordPos);
+                        boolean recordDeleted = checkBit(bitSetView, recordsPerPage + recordPos);
+                        if(recordAvailable && !recordDeleted) {
                             break;
                         }
                         recordPos += 1;
